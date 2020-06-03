3cc02709524fa42d96ae86d34d3f627ed032043b
Theodore Butts <theodorebutts@gmail.com>
2020-06-02 21:07:27 -0600

    create issues list js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -20,7 +20,6 @@ var getUserRepos = function (user) {
             }
         })
         .catch(function (error) {
-            // Notice this `.catch()` getting chained onto the end of the `.then()` method
             alert("Unable to connect to GitHub");
         });
 };
